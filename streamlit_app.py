bbd3ceeea848096c74c994967d381917202942b1
JonGokhale <92873152+JonGokhale@users.noreply.github.com>
2024-06-20 09:57:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ session = cnx.session()
 
 # Retrieve the list of fruit options from the database
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=true)
+st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
 
